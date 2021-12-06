c52bcac694df44e81db7f6063aa19be580eea959
firko-learnin <jackfirkin@gmail.com>
2021-12-06 16:44:09 +0000

    Task 4 complete

--- index.js
+++ index.js
@@ -17,14 +17,15 @@
 //   ];
 
 import myCollection from "./collection.js";
+import chalk from "chalk";
 
 //   console.log(myCollection);
 
   function describeItem (item) {
     if (item.count === 1) {
-        console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`)
+        console.log(`I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)}`)
     } else {
-        console.log(`I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`)
+        console.log(`I have ${chalk.yellow(item.count)} ${chalk.cyan(item.name)}s. Here's what I like about them: ${chalk.green(item.whatILike)}`)
     }
   }
 
