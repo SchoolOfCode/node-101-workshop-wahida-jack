e5140e2139e0734036a6d86a68846d9c7fb7efe3
Wahida Omar <wahidaomar@Wahidas-MacBook-Air.local>
2021-12-06 16:56:07 +0000

    Complete

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ import chalk from "chalk";
 
   function describeItem (item) {
     if (item.count === 1) {
-        console.log(`I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)}`)
+        console.log(`I have a ${chalk.inverse.bgCyan.bold.red(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)}`)
     } else {
-        console.log(`I have ${chalk.yellow(item.count)} ${chalk.cyan(item.name)}s. Here's what I like about them: ${chalk.green(item.whatILike)}`)
+        console.log(`I have ${chalk.yellow(item.count)} ${chalk.strikethrough.cyan(item.name)}s. Here's what I like about them: ${chalk.green(item.whatILike)}`)
     }
   }
 
@@ -40,3 +40,5 @@ console.log(describeItem(array[i]))
 }
 
 console.log(describeCollection(myCollection));
+
+
