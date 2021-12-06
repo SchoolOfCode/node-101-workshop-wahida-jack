e0d5469b66c37b15673688fbd53a6ccfb355bca7
Wahida Omar <wahidaomar@Wahidas-MacBook-Air.local>
2021-12-06 16:30:03 +0000

    Task 3b

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
+// let myCollection = [
+//     {
+//       name: "School of Code mug",
+//       count: 1,
+//       whatILike: "It has my cute pixel character on it!"
+//     },
+//     {
+//       name: "School of Code hat",
+//       count: 2,
+//       whatILike: "An often overlooked fashion accessory"
+//     },
+//     {
+//       name: "School of Code pillow",
+//       count: 1,
+//       whatILike: "Eat. Sleep. Code. Repeat :)"
+//     }
+//   ];
+
+import myCollection from "./collection.";
 
 //   console.log(myCollection);
 
@@ -26,4 +28,14 @@ let myCollection = [
     }
   }
 
-describeItem(myCollection[0]);
+// describeItem(myCollection[0]);
+
+//Task 2d
+
+function describeCollection(array) {
+ for (let i=0; i<myCollection.length;i++) {
+console.log(describeItem(array[i]))
+ }
+}
+
+console.log(describeCollection(myCollection));
